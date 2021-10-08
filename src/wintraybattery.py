0850c0a5aed9a43d1ff139f13df1d341e0b5ee4e
Ethan Zammit <ethanzammit@gmail.com>
2021-10-08 14:38:36 +0200

    setting dynamic framework

--- src/wintraybattery.py
+++ src/wintraybattery.py
@@ -10,13 +10,68 @@ t = wmi.WMI(moniker = "//./root/wmi")
 '''
 Intention is to allow the user to choose which items to show throgh a submenu with checkboxes
 '''
-def get_new_text():
-    print('fethcing new text')
+class metric:
+    def __init__(self, name, unit, value, hidden=False):
+        self.name = name
+        self.unit = unit
+        self.value = value
+        self.hidden = hidden
+    
+    def __str__(self):
+        if self.hidden:
+            return ""
+        else:
+            return self.name + ": " + str(self.value) + self.unit
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.name == other.name and self.unit == other.unit and self.value == other.value
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
+class metric_system:
+    def __init__(self,metrics=[]):
+        self.metrics = metrics
+    
+    def __str__(self):
+        return "\n".join([str(m) for m in self.metrics])
+    
+    def __repr__(self):
+        return self.__str__()
+
+    def __eq__(self, other):
+        return self.metrics == other.metrics
+    
+    def __ne__(self, other):
+        return not self.__eq__(other)
+    
+    def add_metric(self, metric):
+        self.metrics.append(metric)
+    
+    def remove_metric(self, metric):
+        self.metrics.remove(metric)
+    
+    def get_metric(self, name):
+        for m in self.metrics:
+            if m.name == name:
+                return m
+        return None
+    
+    def get_metrics(self):
+        return self.metrics
+    
+
+
+
+def getUpdatedText():
     new_text = ""
     batts = t.ExecQuery('Select * from BatteryStatus where Voltage > 0')
     for i, b in enumerate(batts):
         # new_text+=(f'Battery: {i}\n')
-        # new_text+=('isCharging:       ' + str(b.PowerOnline)) + '\n'
+        new_text+=('isCharging: ' + str(b.PowerOnline)) + '\n'
         # new_text+=('Discharging:       ' + str(b.Discharging)) + '\n'
         # new_text+=('Charging:          ' + str(b.Charging)) + '\n'
         # new_text+=('Voltage:           ' + str(b.Voltage)) + '\n'
@@ -56,14 +111,14 @@ def get_new_text():
 systray = SysTrayIcon("icon.ico", "Example tray icon", None)
 systray.start()
 
+print('Starting Tray Icon...')
 sleep(5) # requires sleep to allow the systray to start
+
+
+print('Starting battery monitor...')
 print(systray._hwnd)
 while systray._hwnd is not None:
-    new_text = get_new_text()
-    print(f'Got new text: {new_text}')
+    new_text = getUpdatedText()
+    print(f'Got new text: \n{new_text}')
     systray.update("icon.ico", new_text)
-    sleep(4)
-
-# while(True):
-#     print(systray._hwnd)
-#     sleep(2)
+    sleep(3)
